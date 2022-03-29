babab5544f86f16c068a722f1dc48ef73e5d1c27
Heather Osborne <theoz308@comcast.net>
2022-03-28 21:40:39 -0500

    update to assignment four

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -8,81 +8,161 @@ console.log('***** Function Practice *****')
 function hello() {
   return 'Hello World!';
 }
-// Call the function to test
+
+hello ();
+
+//Call the function to test
 console.log('Test - should say "Hello World!"', hello());
+console.log ('Test -', hello ())
 
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
+  console.log ('Hello', name)
   return;
 }
-// Remember to call the function to test
 
+
+// Remember to call the function to test
+helloName ( 'Christy !');
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber ) {
+function addNumbers( firstNumber, secondnumber) {
+  console.log (' Add two numbers', firstNumber, secondnumber);
+  let amount = firstNumber + secondnumber;
+  console.log ('amount', amount);
   // return firstNumber + secondNumber;
+  return amount;
 }
-
+addNumbers (1,4);
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( ){
+
+function multiplyThree( num0, num1, num2 ){
+let answer = (num0 * num1 * num2);
+console.log ('answer', answer);
+return answer; 
 
 }
 
+multiplyThree (3,4, 5)
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
   if ( number > 0 ){
-    return;
+    console.log ('true', number)
+    return true;
   }
-    return;
+   else { 
+     console.log ('false', number)
+  return false;
+   }
 }
+
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
 console.log( 'isPositive - should say true', isPositive(3) );
+console.log ('If number is positive 3 =' , isPositive (3));
 console.log( 'isPositive - should say false', isPositive(0) );
+console.log (' If number is zero = ', isPositive (0));
 console.log( 'isPositive - should say false', isPositive(-3) );
+console.log (' If number is negative 3 = ', isPositive (-3));
+
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
-function getLast( array ) {
+// numbers to choose from 
+let numbers = [1 , 2];
 
-}
+// get the last number from the array
+  function getLast (array) {
+
+    console.log('Array last item', array [1]);
+
+
+
+  }
+getLast (numbers);
 
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ){
-  
+
+ 
+
+function find (value, array  ){
+
+ 
+for ( let i=0; i<array.length; i++ ){
+// i<2 can be array.length 
+console.log ('this numbers loop', array );
+if ( value === array[i]) { 
+return true; 
 }
+}
+return false; 
+
+}
+console.log ('found number', find ( 3, numbers ));
 
 // ----------------------
 // Stretch Goals
 // ----------------------
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
+
 function isFirstLetter(letter, string) {
 
 }
 console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
 console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
 
+
+
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
-  let sum = 0
+
+
+function sumAll( addingNumbers) {
+
+let sum = 0
+ 
   // TODO: loop to add items
-  return sum;
+for (i = 0; i<addingNumbers.length; i++) {
+  sum+= addingNumbers[i];
+//
+
 }
+return sum;
+
+}
+
+  console.log ('sum all', sumAll(numbers));
+  
+
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
 
+let myArray = [1, 2 ];
+function allPostive (myArray){
+ 
+ if (myArray > 0) {
+ console.log ('num', myArray );
+return true;  
+}
+else {
 
+console.log ('empty arryay',[]); 
+return false;  
+
+}
+}
+
+allPostive (myArray);
 
 // 11. Pick a problem from Edabit(https://edabit.com/) or 
 //     CodeWars(https://www.codewars.com/). Then describe it 
